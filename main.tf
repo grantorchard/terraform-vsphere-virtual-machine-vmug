4c0a4a19c0782dd7f6d80badeee9c62d23803d04
Grant Orchard <go@hashicorp.com>
2020-08-08 09:26:22 +1000

    Remove module and deploy disk

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+/*
 module "my-vm" {
   source = "github.com/grantorchard/terraform-vsphere-module-virtual-machine"
 
@@ -6,4 +7,14 @@ module "my-vm" {
   networks = {"VM Network":"dhcp"}
   primary_datastore = "hl-core-ds02"
   template = "consul_sso-srv"
+}
+*/
+
+resource vsphere_virtual_disk "this" {
+  size       = 2
+  vmdk_path  = "/tfc-agent/disk0.vmdk"
+  create_directories = true
+  datacenter = "Core"
+  datastore  = "hl-core-ds02"
+  type       = "thin"
 }
