61d954937326ac32820ea3d4c1dec94188b5f489
Grant Orchard <go@hashicorp.com>
2020-08-07 15:07:20 +1000

    agent testing

--- main.tf
+++ main.tf
@@ -1,1 +1,9 @@
+module "my-vm" {
+  source = "github.com/terraform-vsphere-module-virtual-machine"
 
+  datacenter = "Core"
+  cluster = "Management"
+  networks = {"VM Network":"dhcp"}
+  primary_datastore = "hl-core-ds02"
+  template = "consul_sso-srv"
+}
