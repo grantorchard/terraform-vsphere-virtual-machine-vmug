99f0ac1fbb9979548500b0316930d70e5f2b2670
Grant Orchard <go@hashicorp.com>
2020-08-11 12:56:20 +1000

    troubleshooting formatlist behaviour

--- main.tf
+++ main.tf
@@ -1,23 +1,3 @@
-/*module "my-vm" {
-  source = "github.com/grantorchard/terraform-vsphere-module-virtual-machine"
-
-  datacenter = "Core"
-  cluster = "Management"
-  networks = {"VM Network":"dhcp"}
-  primary_datastore = "hl-core-ds02"
-  template = "consul_sso-srv"
-}
-
-
-resource vsphere_virtual_disk "this" {
-  size       = 2
-  vmdk_path  = "/tfc-agent/disk0.vmdk"
-  create_directories = true
-  datacenter = "Core"
-  datastore  = "hl-core-ds02"
-  type       = "thin"
-}
-*/
 module "ovf-deploy" {
   source = "github.com/grantorchard/terraform-vsphere-module-virtual-machine"
 
